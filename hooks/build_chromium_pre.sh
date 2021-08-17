ff84d79d39417d6b5aeefa57958cb818b2918940
CaseyBakey <caseybakey@none.devnull>
2021-08-17 17:30:47 +0200

    Patch Chromium with Bromite patches before build

--- hooks/build_chromium_pre.sh
+++ hooks/build_chromium_pre.sh
@@ -6,3 +6,9 @@
 # git clone https://github.com/GrapheneOS/Vanadium.git "${vanadium_dir}"
 # cd "${CHROMIUM_BUILD_DIR}/src"
 # git am --whitespace=nowarn ${vanadium_dir}/patches/*.patch
+
+echo "applying bromite patches"
+cd "${CHROMIUM_BUILD_DIR}/src"
+rm -rf .git/rebase-apply/
+# Apply the patches in the given order
+while IFS= read -r line; do git am --whitespace=nowarn "${BROMITE_DIR}/build/patches/$line"; done < "${BROMITE_DIR}/build/bromite_patches_list.txt"
